f53b56d57c813ffad05043612e709cd822cf2cab
cattarinn <115492716+cattarinn@users.noreply.github.com>
2023-08-08 17:59:48 +0300

    Chromedriver

--- webscraper_test.py
+++ webscraper_test.py
@@ -7,6 +7,6 @@ Created on Tue Aug  8 16:30:04 2023
 
 import webscraper as wb
 import pandas as pd
-path = 'C:/Users/Катя/Documents/DS_salaries_analysis/chromedriver'
+path = 'C:/Users/Катя/Documents/DS_salaries_analysis/chromedriver.exe'
 
 df = wb.get_jobs('data scientist', 15, False, path, 15)
