2e0d895abf85a90fa7757708bcf3e3de2af99bdb
cattarinn <115492716+cattarinn@users.noreply.github.com>
2023-08-08 17:36:46 +0300

    Uploaded a webscraper and it's testing code

---
+++ webscraper_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  8 16:30:04 2023
+
+@author: Катя
+"""
+
+import webscraper as wb
+import pandas as pd
+path = 'C:/Users/Катя/Documents/DS_salaries_analysis/chromedriver'
+
+df = wb.get_jobs('data scientist', 15, False, path, 15)
